763877c31ad9511b29f2854cccf2ef21d4e5dbbe
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-09-18 13:56:39 +0100

    release

--- scripts/plot/start_here.py
+++ scripts/plot/start_here.py
@@ -125,7 +125,7 @@ __Searches__
 Model-fits using a non-linear search (e.g. Nautilus, Emcee) produce search-specific visualization.
 
 The `autocti.workspace.*.plot.search` illustrates how to perform this visualization for every search (e.g.
-`DynestyPlotter`, `EmceePlotter`.
+`NautilusPlotter`, `EmceePlotter`.
 
 __Adding Plotter Objects Together__
 
